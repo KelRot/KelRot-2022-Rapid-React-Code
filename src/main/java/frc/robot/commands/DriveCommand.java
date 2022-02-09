ae592ef694cf157bcdf38f9d587db0623f13e975
KelRot <56571165+KelRot@users.noreply.github.com>
2022-02-09 09:50:53 +0300

    updated for wpi2022

---
+++ src/main/java/frc/robot/commands/DriveCommand.java
@@ -0,0 +1,41 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.DriveBase;
+
+public class DriveCommand extends CommandBase {
+  /** Creates a new DriveCommand. */
+  private final DriveBase m_drivebase;
+  private final Joystick joystick;
+  public DriveCommand(DriveBase subsystem, Joystick js) {
+    m_drivebase= subsystem;
+    joystick= js;
+    addRequirements(subsystem);
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    m_drivebase.curvatureDrive(joystick);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/DriveBase.java
@@ -0,0 +1,40 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+import edu.wpi.first.wpilibj.motorcontrol.Talon;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import frc.robot.Constants.DriveConstants;
+
+public class DriveBase extends SubsystemBase {
+  /** Creates a new DriveBase. */
+  Talon frontLeft= new Talon(DriveConstants.solOn);
+  Talon backLeft= new Talon(DriveConstants.solArka);
+  Talon frontRight= new Talon(DriveConstants.sagOn);
+  Talon backRight= new Talon(DriveConstants.sagArka);
+
+
+  MotorControllerGroup left= new MotorControllerGroup(frontLeft, backLeft);
+  MotorControllerGroup right= new MotorControllerGroup(frontRight, backRight);
+
+  DifferentialDrive drivetrain = new DifferentialDrive(left, right);
+
+  public DriveBase() {
+    frontRight.setInverted(true);
+    backRight.setInverted(true);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  public void curvatureDrive(Joystick js){
+    drivetrain.curvatureDrive(-js.getRawAxis(1),js.getRawAxis(4),js.getRawButton(5));
+  }
+}
